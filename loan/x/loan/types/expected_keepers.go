03a7162f5fbfe84cd24eaab0a6a1fee43d94790a
django_freeman <o.tampodo.t.18@gmail.com>
2022-12-22 15:27:29 +0900

    LoanをLenderが承認する処理

--- loan/x/loan/types/expected_keepers.go
+++ loan/x/loan/types/expected_keepers.go
@@ -14,4 +14,5 @@ type AccountKeeper interface {
 // BankKeeper defines the expected interface needed to retrieve account balances.
 type BankKeeper interface {
 	SendCoinsFromAccountToModule(ctx sdk.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error
+	SendCoins(ctx sdk.Context, senderAddr sdk.AccAddress, recipientModule sdk.AccAddress, amt sdk.Coins) error
 }
