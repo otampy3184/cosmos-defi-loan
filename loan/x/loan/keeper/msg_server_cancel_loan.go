67fe5d6eb31763dbb8e441a19feb3723200a313f
django_freeman <o.tampodo.t.18@gmail.com>
2022-12-24 04:12:53 +0900

    update

--- loan/x/loan/keeper/msg_server_cancel_loan.go
+++ loan/x/loan/keeper/msg_server_cancel_loan.go
@@ -2,16 +2,21 @@ package keeper
 
 import (
 	"context"
-
-	sdk "github.com/cosmos/cosmos-sdk/types"
+	"fmt"
+   
 	"github.com/username/loan/x/loan/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 func (k msgServer) CancelLoan(goCtx context.Context, msg *types.MsgCancelLoan) (*types.MsgCancelLoanResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
 	// TODO: Handling the message
-	_ = ctx
-
+	loan, found := k.GetLoan(ctx, msg.Id)
+	if !found {
+	 return nil, sdkerrors.Wrap(sdkerrors.ErrKeyNotFound, fmt.Sprintf("key %d doesn't exist", msg.Id))
+	}
+   
 	return &types.MsgCancelLoanResponse{}, nil
 }
