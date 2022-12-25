e2031ac5bd8ddd4ce59480ef7c32ae9eeebad167
django_freeman <o.tampodo.t.18@gmail.com>
2022-12-25 23:49:44 +0900

    Loanキャンセル機能追加

--- loan/x/loan/keeper/msg_server_cancel_loan.go
+++ loan/x/loan/keeper/msg_server_cancel_loan.go
@@ -17,6 +17,22 @@ func (k msgServer) CancelLoan(goCtx context.Context, msg *types.MsgCancelLoan) (
 	if !found {
 	 return nil, sdkerrors.Wrap(sdkerrors.ErrKeyNotFound, fmt.Sprintf("key %d doesn't exist", msg.Id))
 	}
+
+	if loan.Borrower != msg.Creator {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "Cannnot cancel: not the borrower")
+	}
+
+	if loan.State != "requested" {
+		return nil, sdkerrors.Wrapf(types.ErrWrongLoanState, "%v", loan.State)
+	}
+
+	borrower, _ := sdk.AccAddressFromBech32(loan.Borrower)
+	collateral, _ := sdk.ParseCoinsNormalized(loan.Collateral)
+	k.bankKeeper.SendCoinsFromModuleToAccount(ctx, types.ModuleName, borrower, collateral)
+
+	loan.State = "cancelled"
+
+	k.SetLoan(ctx, loan)
    
 	return &types.MsgCancelLoanResponse{}, nil
 }
